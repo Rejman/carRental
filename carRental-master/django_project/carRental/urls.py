14b6b3f4d05fcf6ad81a13358b42e7241c42b1a5
Matyaszek <35657547+Matyaszek@users.noreply.github.com>
2019-02-10 21:09:10 +0100

    Add files via upload

---
+++ carRental-master/django_project/carRental/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="carRental-home"),
+    path('<str:mark>', views.home, name="carRental-home"),
+    path('<int:car_model_id>/desc/', views.desc, name="desc"),
+]
---
+++ carRental-master/django_project/django_project/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
+from users import views as user_views
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('register/', user_views.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+    path('profile/', user_views.profile, name="profile"),
+    path('profile/contact', user_views.contact, name="contact"),
+    path('', include('carRental.urls')),
+]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
